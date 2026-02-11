24587026ba438266dc25d6cfe2cc4f084f124b19
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 16:25:55 -0300

    Update and rename MigracaoView.xaml to MigracaoView.xaml.cs

---
+++ SistemaJuridico/Views/MigracaoView.xaml.cs
@@ -0,0 +1,31 @@
+using SistemaJuridico.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class MigracaoView : UserControl
+    {
+        public MigracaoView()
+        {
+            InitializeComponent();
+            Loaded += MigracaoView_Loaded;
+        }
+
+        private void MigracaoView_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MigracaoViewModel vm)
+            {
+                vm.MigracaoConcluidaComSucesso += Vm_MigracaoConcluidaComSucesso;
+            }
+        }
+
+        private void Vm_MigracaoConcluidaComSucesso()
+        {
+            var main = new MainWindow();
+            main.Show();
+
+            Window.GetWindow(this)?.Close();
+        }
+    }
+}
