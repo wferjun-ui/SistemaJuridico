e1476e40f7a4f1ce7f5a8afa04fd4633fa69c5d7
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:43:44 -0300

    Create ViewFactoryService.cs

---
+++ SistemaJuridico/Services/ViewFactoryService.cs
@@ -0,0 +1,39 @@
+using SistemaJuridico.Infrastructure;
+using System;
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Services
+{
+    public class ViewFactoryService
+    {
+        private readonly ServiceLocator _serviceLocator;
+
+        public ViewFactoryService(ServiceLocator serviceLocator)
+        {
+            _serviceLocator = serviceLocator;
+        }
+
+        public UserControl CreateView<TView, TViewModel>()
+            where TView : UserControl, new()
+        {
+            var view = new TView();
+
+            var vm = _serviceLocator.Get<TViewModel>();
+
+            view.DataContext = vm;
+
+            return view;
+        }
+
+        public UserControl CreateView(Type viewType, Type viewModelType)
+        {
+            var view = (UserControl)Activator.CreateInstance(viewType);
+
+            var vm = _serviceLocator.Get(viewModelType);
+
+            view.DataContext = vm;
+
+            return view;
+        }
+    }
+}
