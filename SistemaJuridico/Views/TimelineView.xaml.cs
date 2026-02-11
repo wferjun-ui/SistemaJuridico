15aeea38ed6f881b2c321ff119ba479b30f69fbf
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:52:53 -0300

    Create TimelineView.xaml.cs

---
+++ SistemaJuridico/Views/TimelineView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class TimelineView : UserControl
+    {
+        public TimelineView()
+        {
+            InitializeComponent();
+        }
+    }
+}
