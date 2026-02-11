3507c6f2a219c7637c00e165f6bd5d5fe45b5350
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:41:43 -0300

    Create MainShellWindow.xaml.cs

---
+++ SistemaJuridico/Views/MainShellWindow.xaml.cs
@@ -0,0 +1,19 @@
+using SistemaJuridico.Services;
+using SistemaJuridico.ViewModels;
+using System.Windows;
+
+namespace SistemaJuridico.Views
+{
+    public partial class MainShellWindow : Window
+    {
+        public MainShellWindow(
+            NavigationService navigationService,
+            MainShellViewModel vm)
+        {
+            InitializeComponent();
+
+            navigationService.Configure(MainRegion);
+            DataContext = vm;
+        }
+    }
+}
