93d2957e9332a09e34103c5dc9fc6854e6438872
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:53:26 -0300

    Create ProcessoTimelineHostView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoTimelineHostView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoTimelineHostView : UserControl
+    {
+        public ProcessoTimelineHostView()
+        {
+            InitializeComponent();
+        }
+    }
+}
