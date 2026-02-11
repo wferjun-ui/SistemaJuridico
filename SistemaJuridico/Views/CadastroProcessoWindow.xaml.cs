b0cfd9e1dd6ac2cb064ed8447937a68c7f5d5de8
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 14:06:17 -0300

    Update CadastroProcessoWindow.xaml.cs

--- SistemaJuridico/Views/CadastroProcessoWindow.xaml.cs
+++ SistemaJuridico/Views/CadastroProcessoWindow.xaml.cs
@@ -1,1 +1,24 @@
+using SistemaJuridico.Services;
+using SistemaJuridico.ViewModels;
+using System.Windows;
+
+namespace SistemaJuridico.Views
+{
+    public partial class CadastroProcessoWindow : Window
+    {
+        public CadastroProcessoWindow()
+        {
+            InitializeComponent();
+
+            var db = new DatabaseService();
+            var service = new ProcessService(db);
+
+            var vm = new CadastroProcessoViewModel(service);
+
+            vm.FecharTela = Close;
+
+            DataContext = vm;
+        }
+    }
+}
 
