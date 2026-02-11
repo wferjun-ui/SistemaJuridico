55d7b77ce61699857a59fb34ff3a4824f8daca81
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:54:50 -0300

    Create ProcessoEdicaoEstadoView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoEdicaoEstadoView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoEdicaoEstadoView : UserControl
+    {
+        public ProcessoEdicaoEstadoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
