c6304758ac57a4338dd9cf6640fb3c5e9cd8accf
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:55:21 -0300

    Create ProcessoMultiusuarioHostView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoMultiusuarioHostView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoMultiusuarioHostView : UserControl
+    {
+        public ProcessoMultiusuarioHostView()
+        {
+            InitializeComponent();
+        }
+    }
+}
