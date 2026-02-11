757475fc43f3b26764d97466ece620cca40eb3a3
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 15:53:13 -0300

    Create MigracaoUsuario.cs

---
+++ SistemaJuridico/Models/MigracaoUsuario.cs
@@ -0,0 +1,10 @@
+namespace SistemaJuridico.Models
+{
+    public class MigracaoUsuario
+    {
+        public string id { get; set; } = "";
+        public string username { get; set; } = "";
+        public string email { get; set; } = "";
+        public int is_admin { get; set; }
+    }
+}
