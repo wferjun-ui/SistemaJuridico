eea78e6cf3822764b4d67eaf0ed61105f6d8c3e5
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:58:37 -0300

    Create ProcessoEditorView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoEditorView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoEditorView : UserControl
+    {
+        public ProcessoEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
