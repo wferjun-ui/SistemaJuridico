39d4178db9fd5e4ed05dbca401e1c1025aae3ecd
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 13:12:49 -0300

    Delete SistemaJuridico directory

--- SistemaJuridico/Models/EmailAutorizado.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaJuridico.Models
-{
-    public class EmailAutorizado
-    {
-        public string Id { get; set; } = "";
-        public string Email { get; set; } = "";
-    }
-}
-
