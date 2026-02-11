ff2f8fdf1022522943efd268768b3a90175e6e30
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:32:42 -0300

    Update AuditLog.cs

--- SistemaJuridico/Models/AuditLog.cs
+++ SistemaJuridico/Models/AuditLog.cs
@@ -3,11 +3,17 @@ namespace SistemaJuridico.Models
     public class AuditLog
     {
         public string Id { get; set; } = "";
+
         public string DataHora { get; set; } = "";
+
         public string Usuario { get; set; } = "";
+
         public string Acao { get; set; } = "";
+
         public string Entidade { get; set; } = "";
+
         public string? EntidadeId { get; set; }
+
         public string? Detalhes { get; set; }
     }
 }
