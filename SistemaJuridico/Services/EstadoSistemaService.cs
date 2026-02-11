65baa61a16ffc693ddea6a7fc6c1d2210229f308
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 16:22:11 -0300

    Create EstadoSistemaService.cs

---
+++ SistemaJuridico/Services/EstadoSistemaService.cs
@@ -0,0 +1,25 @@
+using Dapper;
+using System.Data.SQLite;
+
+namespace SistemaJuridico.Services
+{
+    public class EstadoSistemaService
+    {
+        private readonly DatabaseService _databaseService;
+
+        public EstadoSistemaService(DatabaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+
+        public bool SistemaPossuiDados()
+        {
+            using var conn = _databaseService.GetConnection();
+
+            var totalProcessos = conn.ExecuteScalar<int>(
+                "SELECT COUNT(*) FROM processos");
+
+            return totalProcessos > 0;
+        }
+    }
+}
