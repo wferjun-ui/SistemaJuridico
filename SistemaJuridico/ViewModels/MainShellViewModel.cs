a0a3b8fcfbb11000f40729f2db2386b7d1f0be97
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:47:46 -0300

    Update MainShellViewModel.cs

--- SistemaJuridico/ViewModels/MainShellViewModel.cs
+++ SistemaJuridico/ViewModels/MainShellViewModel.cs
@@ -1,41 +1,33 @@
 using SistemaJuridico.Infrastructure;
 using SistemaJuridico.Services;
-using System.Windows.Controls;
 
 namespace SistemaJuridico.ViewModels
 {
     public class MainShellViewModel : ViewModelBase
     {
-        private readonly NavigationService _navigation;
-        private readonly ViewFactoryService _factory;
-        private readonly ViewRegistryService _registry;
+        private readonly NavigationCoordinatorService _navigator;
 
         public RelayCommand OpenDashboardCommand { get; }
+        public RelayCommand OpenProcessosCommand { get; }
+        public RelayCommand OpenContasCommand { get; }
 
-        public MainShellViewModel(
-            NavigationService navigation,
-            ViewFactoryService factory,
-            ViewRegistryService registry)
+        public MainShellViewModel(NavigationCoordinatorService navigator)
         {
-            _navigation = navigation;
-            _factory = factory;
-            _registry = registry;
+            _navigator = navigator;
 
-            OpenDashboardCommand = new RelayCommand(OpenDashboard);
+            OpenDashboardCommand = new RelayCommand(() =>
+                _navigator.Navigate(NavigationKey.Dashboard));
+
+            OpenProcessosCommand = new RelayCommand(() =>
+                _navigator.Navigate(NavigationKey.Processos));
+
+            OpenContasCommand = new RelayCommand(() =>
+                _navigator.Navigate(NavigationKey.Contas));
         }
 
         public void LoadInitialView()
         {
-            OpenDashboard();
-        }
-
-        private void OpenDashboard()
-        {
-            var (viewType, vmType) = _registry.Resolve("Dashboard");
-
-            var view = _factory.CreateView(viewType, vmType);
-
-            _navigation.Navigate(view);
+            _navigator.Navigate(NavigationKey.Dashboard);
         }
     }
 }
