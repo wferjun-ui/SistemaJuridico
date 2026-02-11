569b3ae77feb18b09ce32a9d68288b6364f8c7c8
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 14:05:51 -0300

    Update CadastroProcessoViewModel.cs

--- SistemaJuridico/ViewModels/CadastroProcessoViewModel.cs
+++ SistemaJuridico/ViewModels/CadastroProcessoViewModel.cs
@@ -1,1 +1,49 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SistemaJuridico.Models;
+using SistemaJuridico.Services;
+using System.Windows;
+
+namespace SistemaJuridico.ViewModels
+{
+    public partial class CadastroProcessoViewModel : ObservableObject
+    {
+        private readonly ProcessService _service;
+
+        public Processo NovoProcesso { get; set; } = new();
+
+        public Action? FecharTela { get; set; }
+
+        public CadastroProcessoViewModel(ProcessService service)
+        {
+            _service = service;
+
+            NovoProcesso.StatusFase = "Conhecimento";
+            NovoProcesso.UltimaAtualizacao =
+                DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        [RelayCommand]
+        private void Salvar()
+        {
+            if (string.IsNullOrWhiteSpace(NovoProcesso.Numero))
+            {
+                MessageBox.Show("Número obrigatório.");
+                return;
+            }
+
+            _service.CriarProcesso(NovoProcesso);
+
+            MessageBox.Show("Processo criado.");
+
+            FecharTela?.Invoke();
+        }
+
+        [RelayCommand]
+        private void Cancelar()
+        {
+            FecharTela?.Invoke();
+        }
+    }
+}
 
