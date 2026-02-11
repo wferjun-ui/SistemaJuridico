2bd815e0873228d6601b774a2d40b8ed667f167e
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 12:52:18 -0300

    Update CadastroUsuarioWindow.xaml.cs

--- Views/CadastroUsuarioWindow.xaml.cs
+++ Views/CadastroUsuarioWindow.xaml.cs
@@ -1,1 +1,22 @@
+using SistemaJuridico.ViewModels;
+using System.Windows;
+
+namespace SistemaJuridico.Views
+{
+    public partial class CadastroUsuarioWindow : Window
+    {
+        public CadastroUsuarioWindow()
+        {
+            InitializeComponent();
+
+            var vm = new CadastroUsuarioViewModel();
+            DataContext = vm;
+
+            PwdBox.PasswordChanged += (s, e) =>
+            {
+                vm.Senha = PwdBox.Password;
+            };
+        }
+    }
+}
 
