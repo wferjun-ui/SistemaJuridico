6a92ca42fc309ad17b3374b60bb25e13c2aad629
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 12:47:27 -0300

    Update EmailAutorizado.cs

--- Models/EmailAutorizado.cs
+++ Models/EmailAutorizado.cs
@@ -1,1 +1,9 @@
+namespace SistemaJuridico.Models
+{
+    public class EmailAutorizado
+    {
+        public string Id { get; set; } = "";
+        public string Email { get; set; } = "";
+    }
+}
 
