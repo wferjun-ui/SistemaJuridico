1c18f3189615f89169b94b4f472d1dc757bc5068
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:50:47 -0300

    Create ProcessoDetalhesHostView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoDetalhesHostView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoDetalhesHostView : UserControl
+    {
+        public ProcessoDetalhesHostView()
+        {
+            InitializeComponent();
+        }
+    }
+}
