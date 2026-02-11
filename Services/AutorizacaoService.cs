4ef4efe8cbd6eb67b061f9e25437606a22628f22
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 12:48:08 -0300

    Implement email authorization service methods

--- Services/AutorizacaoService.cs
+++ Services/AutorizacaoService.cs
@@ -1,1 +1,41 @@
+using Dapper;
+using System;
+
+namespace SistemaJuridico.Services
+{
+    public class AutorizacaoService
+    {
+        private readonly DatabaseService _db;
+
+        public AutorizacaoService(DatabaseService db)
+        {
+            _db = db;
+        }
+
+        public bool EmailAutorizado(string email)
+        {
+            using var conn = _db.GetConnection();
+
+            var count = conn.ExecuteScalar<int>(
+                "SELECT COUNT(*) FROM emails_autorizados WHERE lower(email)=lower(@e)",
+                new { e = email });
+
+            return count > 0;
+        }
+
+        public void AdicionarEmail(string email)
+        {
+            using var conn = _db.GetConnection();
+
+            conn.Execute(@"
+INSERT INTO emails_autorizados(id,email)
+VALUES(@id,@e)",
+                new
+                {
+                    id = Guid.NewGuid().ToString(),
+                    e = email
+                });
+        }
+    }
+}
 
