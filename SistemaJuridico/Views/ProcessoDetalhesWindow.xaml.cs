f7eab1896b5a4e4919995f5bab6d841137973652
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 13:17:34 -0300

    Update ProcessoDetalhesWindow.xaml.cs

--- SistemaJuridico/Views/ProcessoDetalhesWindow.xaml.cs
+++ SistemaJuridico/Views/ProcessoDetalhesWindow.xaml.cs
@@ -1,14 +1,27 @@
 using SistemaJuridico.ViewModels;
+using System.ComponentModel;
 using System.Windows;
 
 namespace SistemaJuridico.Views
 {
     public partial class ProcessoDetalhesWindow : Window
     {
+        private readonly ProcessoDetalhesViewModel _vm;
+
         public ProcessoDetalhesWindow(string processoId)
         {
             InitializeComponent();
-            DataContext = new ProcessoDetalhesViewModel(processoId);
+
+            _vm = new ProcessoDetalhesViewModel(processoId);
+            DataContext = _vm;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (!_vm.PodeFechar())
+                e.Cancel = true;
+
+            base.OnClosing(e);
         }
     }
 }
