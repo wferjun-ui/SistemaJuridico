eee96e8bc95b52efe891e5a989671b741f09c4bc
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 12:49:12 -0300

    Update DashboardWindow.xaml.cs

--- Views/DashboardWindow.xaml.cs
+++ Views/DashboardWindow.xaml.cs
@@ -1,1 +1,17 @@
+using System.Windows;
+
+namespace SistemaJuridico.Views
+{
+    public partial class DashboardWindow : Window
+    {
+        public string Usuario =>
+            $"Logado como: {App.Session.UsuarioAtual?.Email}";
+
+        public DashboardWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
 
