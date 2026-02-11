b303bd54f48c3886265233492e46590d1c01d813
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:49:27 -0300

    Create ProcessoListView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoListView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoListView : UserControl
+    {
+        public ProcessoListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
