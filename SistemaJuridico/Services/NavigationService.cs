4839a352c0e77411ab9276033828a41eee81e27d
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:40:52 -0300

    Create NavigationService.cs

---
+++ SistemaJuridico/Services/NavigationService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Services
+{
+    public class NavigationService
+    {
+        private ContentControl _region;
+
+        public void Configure(ContentControl region)
+        {
+            _region = region;
+        }
+
+        public void Navigate(UserControl view)
+        {
+            if (_region == null)
+                throw new InvalidOperationException("NavigationService não configurado.");
+
+            _region.Content = view;
+        }
+    }
+}
