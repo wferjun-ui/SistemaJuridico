258037f489eacf8e76c3c8e53f0619ca19f15112
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 18:56:25 -0300

    Create AuditoriaView.xaml.cs

---
+++ SistemaJuridico/Views/AuditoriaView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class AuditoriaView : UserControl
+    {
+        public AuditoriaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
