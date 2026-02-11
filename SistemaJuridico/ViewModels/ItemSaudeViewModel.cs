3542d5f3488d631c42613b6f5d8f835e1f152bdb
wferjun-ui <wfer.jun@gmail.com>
2026-02-11 17:23:52 -0300

    Create ItemSaudeViewModel.cs

---
+++ SistemaJuridico/ViewModels/ItemSaudeViewModel.cs
@@ -0,0 +1,140 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using SistemaJuridico.Models;
+
+namespace SistemaJuridico.ViewModels
+{
+    public partial class ItemSaudeViewModel : ObservableObject
+    {
+        public ItemSaude Model { get; }
+
+        public ItemSaudeViewModel(ItemSaude model)
+        {
+            Model = model;
+        }
+
+        // =====================
+        // PROPRIEDADES BINDING
+        // =====================
+
+        public string Id => Model.Id;
+
+        public string ProcessoId
+        {
+            get => Model.ProcessoId;
+            set
+            {
+                if (Model.ProcessoId != value)
+                {
+                    Model.ProcessoId = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Tipo
+        {
+            get => Model.Tipo;
+            set
+            {
+                if (Model.Tipo != value)
+                {
+                    Model.Tipo = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Nome
+        {
+            get => Model.Nome;
+            set
+            {
+                if (Model.Nome != value)
+                {
+                    Model.Nome = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Qtd
+        {
+            get => Model.Qtd;
+            set
+            {
+                if (Model.Qtd != value)
+                {
+                    Model.Qtd = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Frequencia
+        {
+            get => Model.Frequencia;
+            set
+            {
+                if (Model.Frequencia != value)
+                {
+                    Model.Frequencia = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Local
+        {
+            get => Model.Local;
+            set
+            {
+                if (Model.Local != value)
+                {
+                    Model.Local = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string DataPrescricao
+        {
+            get => Model.DataPrescricao;
+            set
+            {
+                if (Model.DataPrescricao != value)
+                {
+                    Model.DataPrescricao = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsDesnecessario
+        {
+            get => Model.IsDesnecessario == 1;
+            set
+            {
+                var novo = value ? 1 : 0;
+                if (Model.IsDesnecessario != novo)
+                {
+                    Model.IsDesnecessario = novo;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool TemBloqueio
+        {
+            get => Model.TemBloqueio == 1;
+            set
+            {
+                var novo = value ? 1 : 0;
+                if (Model.TemBloqueio != novo)
+                {
+                    Model.TemBloqueio = novo;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
