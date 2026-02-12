bca26842a1a0d79fd5a6efbc44671b0ba4e2218a
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 05:27:23 -0300

    Update ViewModelLocator.cs

--- SistemaJuridico/Infrastructure/ViewModelLocator.cs
+++ SistemaJuridico/Infrastructure/ViewModelLocator.cs
@@ -1,11 +1,10 @@
 using SistemaJuridico.Services;
+using SistemaJuridico.Infrastructure;
 
 namespace SistemaJuridico.Infrastructure
 {
     public class ViewModelLocator
     {
-        public static ServiceLocator ServiceLocator { get; set; }
-
         public static T Resolve<T>()
         {
             return ServiceLocator.Get<T>();
