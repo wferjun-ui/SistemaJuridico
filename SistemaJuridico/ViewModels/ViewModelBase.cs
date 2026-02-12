4d5fd4999f26529c4c6fc22aaa9f03a99b4ec549
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 05:21:09 -0300

    Create ViewModelBase.cs

---
+++ SistemaJuridico/ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SistemaJuridico.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
