8999614016656353b0f252b0049ecb80def6827e
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 06:26:34 -0300

    Create DashboardView.xaml.cs

---
+++ SistemaJuridico/Views/SistemaJuridico/Views/DashboardView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
