a250d733eae8c0ddde3070cbaefab24a96edc8a0
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 05:37:17 -0300

    Create RelayCommandGeneric.cs

---
+++ SistemaJuridico/Infrastructure/RelayCommandGeneric.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Windows.Input;
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T?> _execute;
+    private readonly Func<T?, bool>? _canExecute;
+
+    public RelayCommand(Action<T?> execute, Func<T?, bool>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute == null || _canExecute((T?)parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute((T?)parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void Raise()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
