30792f04ccadd44fe8d64ae015a93e37c52eea22
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 09:03:53 -0300

    Update Diligencia.cs

--- SistemaJuridico/Models/Diligencia.cs
+++ SistemaJuridico/Models/Diligencia.cs
@@ -3,18 +3,12 @@ namespace SistemaJuridico.Models
     public class Diligencia
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-
         public string ProcessoId { get; set; } = "";
-
         public string Descricao { get; set; } = "";
-
-        public string DataCriacao { get; set; } =
-            DateTime.Now.ToString("dd/MM/yyyy");
-
+        public string DataCriacao { get; set; } = DateTime.Now.ToString("o");
         public string? DataConclusao { get; set; }
-
-        public bool Concluida { get; set; } = false;
-
+        public bool Concluida { get; set; }
         public string Responsavel { get; set; } = "";
+        public string? Prazo { get; set; }
     }
 }
