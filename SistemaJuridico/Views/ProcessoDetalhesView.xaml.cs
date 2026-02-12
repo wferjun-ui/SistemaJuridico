df58002f6447122e1943b057c10124a5e2cbf588
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 09:22:54 -0300

    Create ProcessoDetalhesView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoDetalhesView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoDetalhesView : UserControl
+    {
+        public ProcessoDetalhesView()
+        {
+            InitializeComponent();
+        }
+  
