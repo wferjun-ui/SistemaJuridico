1643630739b8aad2d5559ba0ad93034abbd6ac58
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 14:50:47 -0300

    Fix ambiguous KeyEventArgs in login window

--- SistemaJuridico/Presentation/Views/LoginWindow.xaml.cs
+++ SistemaJuridico/Presentation/Views/LoginWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace SistemaJuridico.Views
         }
 
 
-        private void LoginWindow_KeyDown(object sender, KeyEventArgs e)
+        private void LoginWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key != Key.Enter)
                 return;
