1edf72104b3da918a4686df95ead251ffe17e9ac
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 06:27:27 -0300

    Create ProcessoEditorHostView.xaml.cs

---
+++ SistemaJuridico/Views/ProcessoEditorHostView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SistemaJuridico.Views
+{
+    public partial class ProcessoEditorHostView : UserControl
+    {
+        public ProcessoEditorHostView()
+        {
+            InitializeComponent();
+        }
+    }
+}
