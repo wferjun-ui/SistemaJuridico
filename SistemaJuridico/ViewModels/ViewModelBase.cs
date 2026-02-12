6b101a54182a38a9daeac649425c6c4cba98033a
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 09:14:10 -0300

    Update ViewModelBase.cs

--- SistemaJuridico/ViewModels/ViewModelBase.cs
+++ SistemaJuridico/ViewModels/ViewModelBase.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -10,6 +11,16 @@ namespace SistemaJuridico.ViewModels
         protected void OnPropertyChanged([CallerMemberName] string? name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(name);
+            return true;
         }
     }
 }
