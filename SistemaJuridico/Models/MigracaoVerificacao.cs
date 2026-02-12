a4aa0a4bb7d19ce7c3201a23bf2871d24aa585ca
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 09:04:56 -0300

    Update MigracaoVerificacao.cs

--- SistemaJuridico/Models/MigracaoVerificacao.cs
+++ SistemaJuridico/Models/MigracaoVerificacao.cs
@@ -9,5 +9,12 @@ namespace SistemaJuridico.Models
         public string responsavel { get; set; } = "";
         public int diligencia_pendente { get; set; }
         public string pendencias_descricao { get; set; } = "";
+        public int diligencia_realizada { get; set; }
+        public string? diligencia_descricao { get; set; }
+        public string? prazo_diligencia { get; set; }
+        public string? proximo_prazo_padrao { get; set; }
+        public string? data_notificacao { get; set; }
+        public string? alteracoes_texto { get; set; }
+        public string? itens_snapshot_json { get; set; }
     }
 }
