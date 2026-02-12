d8053e46a0b378e67b4c019c37ac9a4610f6230d
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 09:12:19 -0300

    Update VerificacaoService.cs

--- SistemaJuridico/Services/VerificacaoService.cs
+++ SistemaJuridico/Services/VerificacaoService.cs
@@ -1,6 +1,46 @@
+using Dapper;
+using SistemaJuridico.Models;
+
 namespace SistemaJuridico.Services
 {
     public class VerificacaoService
     {
+        private readonly DatabaseService _db;
+
+        public VerificacaoService(DatabaseService db)
+        {
+            _db = db;
+        }
+
+        public List<Verificacao> ListarPorProcesso(string processoId)
+        {
+            using var conn = _db.GetConnection();
+            return conn.Query<Verificacao>(@"
+SELECT
+  id as Id,
+  processo_id as ProcessoId,
+  data_hora as DataHora,
+  status_processo as StatusProcesso,
+  responsavel as Responsavel,
+  diligencia_pendente as DiligenciaPendente,
+  pendencias_descricao as PendenciaDescricao,
+  proximo_prazo_padrao as ProximoPrazo,
+  diligencia_descricao as DiligenciaDescricao,
+  itens_snapshot_json as ItensSnapshotJson
+FROM verificacoes
+WHERE processo_id = @processoId
+ORDER BY data_hora DESC
+", new { processoId }).ToList();
+        }
+
+        public void Inserir(Verificacao verificacao)
+        {
+            using var conn = _db.GetConnection();
+            conn.Execute(@"
+INSERT INTO verificacoes
+(id, processo_id, data_hora, status_processo, responsavel, diligencia_pendente, pendencias_descricao, proximo_prazo_padrao, diligencia_descricao, itens_snapshot_json)
+VALUES
+(@Id, @ProcessoId, @DataHora, @StatusProcesso, @Responsavel, @DiligenciaPendente, @PendenciaDescricao, @ProximoPrazo, @DiligenciaDescricao, @ItensSnapshotJson)", verificacao);
+        }
     }
 }
