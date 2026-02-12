15d8a01aaa9f5b9f5e906b78a94dec27cccd408e
wferjun-ui <wfer.jun@gmail.com>
2026-02-12 09:06:05 -0300

    Update Verificacao.cs

--- SistemaJuridico/Models/Verificacao.cs
+++ SistemaJuridico/Models/Verificacao.cs
@@ -3,19 +3,17 @@ namespace SistemaJuridico.Models
     public class Verificacao
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-
         public string ProcessoId { get; set; } = "";
-
         public string DataHora { get; set; } = "";
-
         public string StatusProcesso { get; set; } = "";
-
         public string Responsavel { get; set; } = "";
-
         public bool DiligenciaPendente { get; set; }
-
         public string? PendenciaDescricao { get; set; }
-
         public string? ProximoPrazo { get; set; }
+        public string? DiligenciaDescricao { get; set; }
+        public string? ItensSnapshotJson { get; set; }
+
+        public string Status { get => StatusProcesso; set => StatusProcesso = value; }
+        public string? Descricao { get => PendenciaDescricao; set => PendenciaDescricao = value; }
     }
 }
