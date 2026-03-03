41994ec41ca50c738464401d391393cec99d75a6
wferjun-ui <wfer.jun@gmail.com>
2026-03-03 12:40:04 -0300

    Fix ambiguous WPF type references in build

--- SistemaJuridico/Application/ViewModels/VerificacaoEditorViewModel.cs
+++ SistemaJuridico/Application/ViewModels/VerificacaoEditorViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
+using Brush = System.Windows.Media.Brush;
 
 namespace SistemaJuridico.ViewModels
 {
